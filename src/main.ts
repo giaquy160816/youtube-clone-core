36c62b6257ba56891e49b10ba6025c229c79000d
quocviet1501 <quocviet1501@gmail.com>
2025-06-09 20:12:53 +0700

    off timeout

--- src/main.ts
+++ src/main.ts
@@ -9,60 +9,63 @@ import { DocumentBuilder } from '@nestjs/swagger';
 import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.use(cookieParser());
-    app.useGlobalPipes(new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-    }));
-    app.useGlobalInterceptors(new TimeoutInterceptor());
-    // Allow all origins for CORS
+  const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+//   app.useGlobalInterceptors(new TimeoutInterceptor());
+  // Allow all origins for CORS
 
-    const configService = app.get(ConfigService);
-    app.enableCors({
-        origin: configService.get<string[]>('cors'),
-        methods: 'GET,HEAD,PUT,POST,DELETE',
-        credentials: true,
-    });
+  const configService = app.get(ConfigService);
+  app.enableCors({
+    origin: configService.get<string[]>('cors'),
+    methods: 'GET,HEAD,PUT,POST,DELETE',
+    credentials: true,
+  });
 
-    const url = configService.get<string>('rabbitmq.url');
-    const queue = configService.get<string>('rabbitmq.queue');
+  const url = configService.get<string>('rabbitmq.url');
+  const queue = configService.get<string>('rabbitmq.queue');
 
-    if (!url || !queue) {
-        throw new Error('RabbitMQ configuration is missing');
-    }
+  if (!url || !queue) {
+    throw new Error('RabbitMQ configuration is missing');
+  }
 
-    app.connectMicroservice(
-        {
-            transport: Transport.RMQ,
-            options: {
-                urls: [url],
-                queue: queue,
-                queueOptions: {
-                    durable: false,
-                },
-            },
-        },
-    );
-    await app.startAllMicroservices();
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [url],
+      queue: queue,
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  await app.startAllMicroservices();
 
-    const config = new DocumentBuilder()
-        .setTitle('API - Project Youtube Clone')
-        .setDescription('API - Xây dựng ứng dụng Youtube Clone với NestJS')
-        .setVersion('1.0')
-        .addBearerAuth({
-            type: 'http',
-            scheme: 'bearer',
-            bearerFormat: 'JWT',
-            description: 'JWT Authorization header using the Bearer scheme',
-            in: 'header',
-            name: 'Authorization',
-        }, 'access_token')
-        .build();
-    const documentFactory = () => SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, documentFactory);
+  const config = new DocumentBuilder()
+    .setTitle('API - Project Youtube Clone')
+    .setDescription('API - Xây dựng ứng dụng Youtube Clone với NestJS')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        description: 'JWT Authorization header using the Bearer scheme',
+        in: 'header',
+        name: 'Authorization',
+      },
+      'access_token',
+    )
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
-    const port = configService.get<number>('port') || 3000;
-    await app.listen(port);
+  const port = configService.get<number>('port') || 3000;
+  await app.listen(port);
 }
 bootstrap();
